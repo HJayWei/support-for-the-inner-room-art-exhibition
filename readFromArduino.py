f9fe21a4daa69f2141be2df44c6724be5ab5c65f
Jay Wei <jay0935199@gmail.com>
2019-05-12 03:45:07 +0800

    完成列印功能

--- readFromArduino.py
+++ readFromArduino.py
@@ -9,11 +9,88 @@ import datetime
 import RPi.GPIO as GPIO
 import MFRC522
 import signal
+import argparse
+import platform
+import struct
+import os
+import inspect
+import textwrap
+import usb.core
+import usb.util
+
+example_strings = [ 
+'------------------------------',
+'Admission time: ',
+'Appearing time: ',
+'Thank you for your attention.']
+
+# USB specific constant definitions
+PIPSTA_USB_VENDOR_ID = 0x0483
+PIPSTA_USB_PRODUCT_ID = 0xA19D
+AP1400_USB_PRODUCT_ID = 0xA053
+AP1400V_USB_PRODUCT_ID = 0xA19C
+
+valid_usb_ids = {PIPSTA_USB_PRODUCT_ID, AP1400_USB_PRODUCT_ID, AP1400V_USB_PRODUCT_ID}
+
+class printer_finder(object):
+    def __call__(self, device):
+        if device.idVendor != PIPSTA_USB_VENDOR_ID:
+            return False
+
+        return True if device.idProduct in valid_usb_ids else False
+
+
+CR = b'\x0D'
+PIPSTA_LINE_CHAR_WIDTH = 32
+FEED_PAST_TEAR_BAR = b'\n' * 5
+
+def setup_usb():
+    '''Connects to the 1st Pipsta found on the USB bus'''
+    # Find the Pipsta's specific Vendor ID and Product ID (also known as vid
+    # and pid)
+    dev = usb.core.find(custom_match=printer_finder())
+    if dev is None:                 # if no such device is connected...
+        raise IOError('Printer not found')  # ...report error
+
+    try:
+        dev.reset()
+
+        # Initialisation. Passing no arguments sets the configuration to the
+        # currently active configuration.
+        dev.set_configuration()
+    except usb.core.USBError as err:
+        raise IOError('Failed to configure the printer', err)
+
+    # Get a handle to the active interface
+    cfg = dev.get_active_configuration()
+
+    interface_number = cfg[(0, 0)].bInterfaceNumber
+    usb.util.claim_interface(dev, interface_number)
+    alternate_setting = usb.control.get_interface(dev, interface_number)
+    intf = usb.util.find_descriptor(
+        cfg, bInterfaceNumber=interface_number,
+        bAlternateSetting=alternate_setting)
+
+    ep_out = usb.util.find_descriptor(
+        intf,
+        custom_match=lambda e:
+        usb.util.endpoint_direction(e.bEndpointAddress) ==
+        usb.util.ENDPOINT_OUT
+    )
+
+    if ep_out is None:  # check we have a real endpoint handle
+        raise IOError('Could not find an endpoint to print to')
+    
+    return ep_out
 
 # Serial information
 COM_PORT = '/dev/ttyACM0'
 BAUD_RATES = 9600
 ser = serial.Serial(COM_PORT, BAUD_RATES)
+
+def epochToStr(epoch):
+  epochToStr = datetime.datetime.fromtimestamp(epoch)
+  return "   " + epochToStr.strftime('%Y-%m-%d %H:%M:%S') + '\n'
 
 def inseToJSON(filename, getRFIDtag, timeToValue):
   with open(filename) as file:
@@ -35,6 +112,21 @@ def inseToJSON(filename, getRFIDtag, timeToValue):
             del getList[len(getList)-1]
             getList.append([timeTotal])
             print("timeDiff: ", timeDiff)
+            # Print image
+            os.system('lp ./logo/Black.jpg')
+            time.sleep(3)
+            pipsta = setup_usb()
+            admission = epochToStr(getTag[getTagLen-1])
+            appearing = epochToStr(getTag[getTagLen-2])
+            example_strings.insert(2,admission)
+            example_strings.insert(4,appearing)
+            example_strings.insert(5,"Total time: " + str(timeDiff) + "s\n")
+
+            for line in example_strings:
+                print (textwrap.fill(line, PIPSTA_LINE_CHAR_WIDTH))
+                pipsta.write(textwrap.fill(line, PIPSTA_LINE_CHAR_WIDTH))
+                pipsta.write(CR)
+            pipsta.write(FEED_PAST_TEAR_BAR)
           noTagNum=False
       # Add new RFID tag
       if noTagNum:
@@ -65,7 +157,7 @@ MIFAREReader = MFRC522.MFRC522()
 try:
   while continue_reading:
     getNowTime = datetime.datetime.now()
-    timeToValue = int(getNowTime.hour) * 3600 + int(getNowTime.minute) * 60 + int(getNowTime.second)
+    timeToValue = int(time.mktime(getNowTime.timetuple()))
     filename = "./recordTime.json"
     # Get arduino information
     while ser.in_waiting:
@@ -79,7 +171,6 @@ try:
 
       # Update now value in recordTime.json
       inseToJSON(filename, getRFIDtag, timeToValue)
-
 
       # Scan for cards    
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
